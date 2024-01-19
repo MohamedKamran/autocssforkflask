491e852ef380cc548c61066db6b404ce94295314
Adithya <adithyaps929@gmail.com>
2024-01-19 17:45:30 +0530

    commit

--- auto css gen.py
+++ auto css gen.py
@@ -1,1 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+
+@app.route("/",methods=['POST','GET'])
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/render",methods=['POST','GET'])
+def render():
+    return render_template("render.html")
+
+@app.route("/render/colouredbox",methods=['POST','GET'])
+def cbox():
+    width,height,color,round,ele="","","","",""
+    if request.method=="POST":
+        width=request.form['width']
+        height=request.form['height']
+        color=request.form['color']
+        round=request.form['round']  
+        ele=request.form['ele']
+    return render_template("colouredbox.html",width=width,height=height,color=color,round=round,ele=ele)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
